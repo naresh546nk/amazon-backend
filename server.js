502f2bb942d750d5c96f183cead95966f46dc721
naresh546nk <91820009+naresh546nk@users.noreply.github.com>
2023-02-03 09:16:14 +0530

    added jwttoken and user signin

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import seedRouter from "./routes/seedRoute.js";
 import productRouter from "./routes/productRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+//import jwt from "jsonwebtoken";
 
 dotenv.config();
 
@@ -15,12 +17,15 @@ mongoose
   .catch((err) => console.log(err.message));
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api/seed", seedRouter);
 app.use("api/products", productRouter);
+app.use("/api/users", userRouter);
 
-//test
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
@@ -28,3 +33,15 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
 });
+
+// const adminUser = {
+//   email: "admin@example.com",
+//   password: "12345",
+// };
+
+// const jwtToken = jwt.sign(
+//   JSON.stringify(adminUser),
+//   process.env.JWT_SECRET,
+//   "60 * 30"
+// );
+// console.log("token :", jwtToken);
